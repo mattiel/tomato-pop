1b7ccb3b6e6770ba14e2f491526df4f3b9c38f38
Mattie Lee <57819411+mattiel@users.noreply.github.com>
2021-06-08 14:51:22 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html']
+  },
+  // [
+  //   './src/**/*.html',
+  //   './src/**/*.js',
+  // ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
